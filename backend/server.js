c255221f05038dbc1366aa1d57f26212ea8835ff
AdarshC0de <adarsh2213student@gmail.com>
2026-02-26 13:49:20 +0530

    be fix 2

--- backend/server.js
+++ backend/server.js
@@ -24,4 +24,8 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use("/api/photos", photoRoutes);
 
+console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API Key:", process.env.CLOUDINARY_API_KEY);
+console.log("API Secret:", process.env.CLOUDINARY_API_SECRET);
+
 app.listen(5000, () => console.log("Server running on port 5000"));
